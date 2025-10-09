90a3f7d13935c356d15902c7d7435870da0af6e2
behnooshbhy2002 <behnoosh.bhy@gmail.com>
2025-10-09 11:25:59 +0330

    set specific time for reminders

--- src/scheduler.py
+++ src/scheduler.py
@@ -11,12 +11,12 @@ def setup_scheduler(app):
     
     app.job_queue.run_daily(
         send_daily_task_reminder,
-        time=time(hour=11, minute=22, tzinfo=iran_tz),
+        time=time(hour=9, minute=0, tzinfo=iran_tz),
         name="daily_task_reminder"
     )
     
     app.job_queue.run_daily(
         send_sleep_reminder,
-        time=time(hour=11, minute=22, tzinfo=iran_tz),
+        time=time(hour=10, minute=0, tzinfo=iran_tz),
         name="sleep_reminder"
     )
